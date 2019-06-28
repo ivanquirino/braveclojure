7a9ac7adcb83b137a4878bdf1b1d66e8c13bb0fc
Ivan Quirino <ivanquirino1928@gmail.com>
2019-06-28 14:14:53 -0300

    chapter 3 exercices

--- src/braveclojure/core.clj
+++ src/braveclojure/core.clj
@@ -1,45 +1,23 @@
 (ns braveclojure.core
   (:gen-class))
 
-(def asym-hobbit-body-parts [{:name "head" :size 3}
-                             {:name "left-eye" :size 1}
-                             {:name "left-ear" :size 1}
-                             {:name "mouth" :size 1}
-                             {:name "nose" :size 1}
-                             {:name "neck" :size 2}
-                             {:name "left-shoulder" :size 3}
-                             {:name "left-upper-arm" :size 3}
-                             {:name "chest" :size 10}
-                             {:name "back" :size 10}
-                             {:name "left-forearm" :size 3}
-                             {:name "abdomen" :size 6}
-                             {:name "left-kidney" :size 1}
-                             {:name "left-hand" :size 2}
-                             {:name "left-knee" :size 2}
-                             {:name "left-thigh" :size 4}
-                             {:name "left-lower-leg" :size 3}
-                             {:name "left-achilles" :size 1}
-                             {:name "left-foot" :size 2}])
+; 2. 
+(defn add100 
+  "Add 100 to a number"
+  [value] (+ value 100)
+)
 
-(defn matching-part
-  [part]
-  {:name (clojure.string/replace (:name part) #"^left-" "right-")
-   :size (:size part)})
-
-(defn symmetrize-body-parts
-  "Expects a seq of maps that have a :name and :size"
-  [asym-body-parts]
-  (loop [remaining-asym-parts asym-body-parts
-         final-body-parts []]
-    (if (empty? remaining-asym-parts)
-      final-body-parts
-      (let [[part & remaining] remaining-asym-parts]
-        (recur remaining
-               (into final-body-parts
-                     (set [part (matching-part part)])))))))
-
+; 3.
+(defn dec-maker
+  "decrement maker"
+  [value] #(- % value)
+)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println (symmetrize-body-parts asym-hobbit-body-parts)))
+  (println "2. add 100 to value 3")
+  (println (add100 3))
+  (println "3. dec-maker dec9 value 10")
+  (let [dec9 (dec-maker 9)] (println (dec9 10)))
+)
